26547af838e0f2828d4d633ebc6886ab1e35738d
wrc042 <wrc0326@outlook.com>
2024-01-12 02:31:04 +0800

    [FIX BUG IN INITIAL GUESS]

--- fmmtl/MagneticFMM.cpp
+++ fmmtl/MagneticFMM.cpp
@@ -50,7 +50,7 @@ void SolvePotential(const void *positions, const void *normals,
                (Hext_[i * 3 + 0] * normals_[i * 3 + 0] +
                 Hext_[i * 3 + 1] * normals_[i * 3 + 1] +
                 Hext_[i * 3 + 2] * normals_[i * 3 + 2]);
-        u[i] = b[i] / (1 + lambda);
+        u[i] = b[i] / (1 - lambda);
     }
 
 #pragma omp parallel for
